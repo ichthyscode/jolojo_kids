941f875e6e069478cbe9baeafdf2b6f42a7277e7
Lim <limsung.kang@gmail.com>
2023-03-02 01:10:22 +0100

    adding a Listview and start to build a profile widget

--- lib/main.dart
+++ lib/main.dart
@@ -10,12 +10,49 @@ class SimpleAnimation extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: RiveAnimation.network(
-          'https://cdn.rive.app/animations/vehicles.riv',
-        ),
+    const title = 'Family';
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(title),
+        backgroundColor: Colors.cyanAccent,
+      ),
+      body: ListView(
+        children: <Widget>[
+          Container(
+            height: 50,
+            child: Center(
+              child: Text('Nice'),
+            ),
+          ),
+          Container(
+            height: 100,
+            child: Center(
+              child: RiveAnimation.network(
+                'https://cdn.rive.app/animations/vehicles.riv',
+              ),
+            ),
+          ),
+        ],
       ),
     );
+  }
+}
+
+class Profile extends StatelessWidget {
+  final String name;
+  final int age;
+  final Image image;
+
+  const Profile({
+    Key? key,
+    required this.name,
+    required this.age,
+    required this.image,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
   }
 }
